97d5b07882d38db4ff8a42efdcd999ced05283e6
adndvlp <andngdv.lpr@gmail.com>
2025-08-23 16:58:19 -0600

    env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     // Configure the proxy to forward API requests to your Express server
     proxy: {
